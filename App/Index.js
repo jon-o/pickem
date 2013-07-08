034ed484601a269b9e340460dab1819cef1be0e9
Jason <jason.d.ody@gmail.com>
2013-07-07 22:05:34 -0400

    Temporary rollback

--- App/Index.js
+++ App/Index.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var path = require("path");
 //var games = require("./Routes/Games.js");
-var controller = require("./Controller.js");
+//var controller = require("./Controller.js");
 
 var app = express();
 
@@ -12,7 +12,7 @@ app.configure(function() {
 app.get('/', function(req, res) {
     res.redirect('/index.html');
 });
-app.get('/api/picks/season/:seasonId/round/:roundId', controller.findPicksForRound);
+//app.get('/api/picks/season/:seasonId/round/:roundId', controller.findPicksForRound);
 
 app.listen(process.env.PORT, process.env.IP);
 

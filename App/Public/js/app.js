2b9d2cadc46930458c8713942e7e1958473ceb52
Jason <jason.d.ody@gmail.com>
2013-07-19 14:53:47 -0400

    Integrated AngularStrap library to handle highlighting active link in navbar

--- App/Public/js/app.js
+++ App/Public/js/app.js
@@ -1,11 +1,10 @@
 'use strict';
 
-var pickem = angular.module('pickem', [])
+var pickem = angular.module('pickem', ['$strap.directives'])
     .config(['$routeProvider', function($routeProvider) {
         
     $routeProvider
-        .when('/games', { templateUrl: 'partials/games-partial.html',
-            }
-        )
+        .when('/games', { templateUrl: 'partials/games-partial.html' })
+        .when('/leaderboard', { templateUrl: 'partials/leaderboard-partial.html' })
         .otherwise({ redirectTo: '/games' });
     }]);

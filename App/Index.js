85640ce1820286e77216b86ba92f6cf47b103705
Jason <jason.d.ody@gmail.com>
2013-07-19 02:05:01 -0400

    Integrated Nodetime add-on

--- App/Index.js
+++ App/Index.js
@@ -1,3 +1,11 @@
+//Setup for Nodetime add-on
+if (process.env.NODETIME_ACCOUNT_KEY) {
+    require('nodetime').profile({
+        accountKey: process.env.NODETIME_ACCOUNT_KEY,
+        appName: 'Pickem' // optional
+    });
+}
+
 var express = require("express");
 var path = require("path");
 //var games = require("./Routes/Games.js");

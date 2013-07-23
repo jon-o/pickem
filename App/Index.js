1c9bb85bd9755a22eede9b6b09d82e6aa77493fe
Jason <jason.d.ody@gmail.com>
2013-07-23 17:41:59 -0400

    Configured express to handle 404s and 500s

--- App/Index.js
+++ App/Index.js
@@ -14,11 +14,29 @@ var adminController = require("./AdminController.js");
 
 var app = express();
 
+var notFoundHandler = function(req, res) {
+    res.statusCode = 404;
+    res.description = 'Not found';
+    res.render('notFound');
+};
+
+var errorHandler = function(err, req, res, next) {
+    console.log("****ERROR****");
+    console.log(err);
+    
+    res.statusCode = 500;
+    res.description = 'Something went wrong';
+    res.render('error');
+};
+
 app.configure(function() {
     app.set('view engine', 'jade');
     app.set('views', __dirname + '/Views');
     app.use(express.static(path.join(__dirname, 'Public')));
     app.use(express.bodyParser());
+    app.use(app.router); //Enable error handling
+    app.use(notFoundHandler); //If no routes match, this will be called
+    app.use(errorHandler); //Express knows a method with 4 params, is for handling errors
 });
 
 app.get('/', function(req, res) {

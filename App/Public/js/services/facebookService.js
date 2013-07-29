eab8340cb81dc37170e1c710f15dc00fbad43e16
Jason <jason.d.ody@gmail.com>
2013-07-29 12:31:38 -0400

    Added pickemService and facebookService js files

---
+++ App/Public/js/services/facebookService.js
@@ -0,0 +1,7 @@
+'use strict';
+
+pickem.factory('facebookService', function() {
+    return {
+        
+    };
+});
---
+++ App/Public/js/services/pickemService.js
@@ -0,0 +1,7 @@
+'use strict';
+
+pickem.factory('pickemService', function() {
+    return {
+        
+    };
+});

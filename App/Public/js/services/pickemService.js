0f1dcd7e8d1d82d3727455e3ae068992ffdd741f
OpenShift guest <cyphertext1@hotmail.com>
2013-07-29 16:06:43 -0400

    Initial current round games display

--- App/Public/js/services/pickemService.js
+++ App/Public/js/services/pickemService.js
@@ -1,7 +1,19 @@
 'use strict';
 
-pickem.factory('pickemService', function() {
+pickem.factory('pickemService', function($q, $http) {
     return {
-        
+        getCurrentRoundGames: function () {
+            var deferred = $q.defer();
+            
+            $http({method: 'GET', url: '/api/picks/season/1'})
+                .success(function (data) {
+                    deferred.resolve(data);
+                })
+                .error(function (data, status) {
+                   deferred.reject(status); 
+                });
+                
+            return deferred.promise;
+        }
     };
 });

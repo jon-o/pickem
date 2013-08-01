6889f1d34fcd02698239ba8dc49cfda5b49520e4
OpenShift guest <cyphertext1@hotmail.com>
2013-08-01 16:53:02 -0400

    Handling of invalid request responses

--- App/Public/js/controllers/gamesController.js
+++ App/Public/js/controllers/gamesController.js
@@ -1,9 +1,21 @@
 'use strict';
 
 pickem.controller('GamesController', 
-    function GamesController($scope, pickemService) {
-        $scope.message = 'Booya!'
+    function GamesController($scope, pickemService) {            
+        $scope.selectedRound = pickemService.getCurrentRoundGames();             
         
-        $scope.selectedRound = pickemService.getCurrentRoundGames();
+        $scope.selectedRound.then(function(response) {
+            if(response.valid) {
+                $scope.games = response.round.games;
+                $scope.roundTitle = response.season.name + ' ' + response.round.text;
+            } else {
+                $scope.errorMessage = handleError(response.message);
+            }
+        });
+        
     }
 );
+
+var handleError = function (errorMessage) {
+    return 'Oooops...something went wrong - ' + errorMessage;
+};

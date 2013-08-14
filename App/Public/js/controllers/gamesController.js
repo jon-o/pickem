3c2ad36b6af842b4eaad9a61343ec6ff33794194
OpenShift guest <cyphertext1@hotmail.com>
2013-08-14 16:14:45 -0400

    Refactored gamesController.js to reduce code duplication

--- App/Public/js/controllers/gamesController.js
+++ App/Public/js/controllers/gamesController.js
@@ -3,56 +3,46 @@
 pickem.controller('GamesController', 
     function GamesController($scope, pickemService) {
         var previousRoundUri;
-        var nextRoundUri;
+        var nextRoundUri;            
         
         $scope.selectedRound = pickemService.rounds.getCurrentRoundGames(1);             
         
         $scope.selectedRound.then(function(response) {
-            if(response.valid) {
-                $scope.games = response.round.games;
-                $scope.roundTitle = response.season.name + ' ' + response.round.text;
-                previousRoundUri = response.round.navigation.previousUri;
-                nextRoundUri = response.round.navigation.nextUri;
-                $scope.errorMessage = '';
-            } else {
-                $scope.errorMessage = handleError(response.message);
-            }
+            processRoundResponse(response);
         });
         
-        $scope.nextRound = function () {
-            $scope.selectedRound = pickemService.rounds.getRoundGames(nextRoundUri);
+        $scope.nextRound = function () {            
+            performGetRoundGames(nextRoundUri);
+        };
+        
+        $scope.previousRound = function () {        
+            performGetRoundGames(previousRoundUri);
+        };
+        
+        var performGetRoundGames = function (getRoundGamesUri) {
+            $scope.selectedRound = pickemService.rounds.getRoundGames(getRoundGamesUri);
             
             $scope.selectedRound.then(function(response) {
-                if(response.valid) {
-                    $scope.games = response.round.games;
-                    $scope.roundTitle = response.season.name + ' ' + response.round.text;
-                    previousRoundUri = response.round.navigation.previousUri;
-                    nextRoundUri = response.round.navigation.nextUri;
-                    $scope.errorMessage = '';
-                } else {
-                    $scope.errorMessage = handleError(response.message);
-                }
+                processRoundResponse(response);
             });
         };
         
-        $scope.previousRound = function () {
-            $scope.selectedRound = pickemService.rounds.getRoundGames(previousRoundUri);
-            
-            $scope.selectedRound.then(function(response) {
-                if(response.valid) {
-                    $scope.games = response.round.games;
-                    $scope.roundTitle = response.season.name + ' ' + response.round.text;
-                    previousRoundUri = response.round.navigation.previousUri;
-                    nextRoundUri = response.round.navigation.nextUri;
-                    $scope.errorMessage = '';
-                } else {
-                    $scope.errorMessage = handleError(response.message);
-                }
-            });        
-        };
+        var processRoundResponse = function (response) {
+            if(response.valid) {
+                $scope.games = response.round.games;
+                $scope.roundTitle = response.season.name + ' ' + response.round.text;
+                $scope.errorMessage = '';
+                
+                previousRoundUri = response.round.navigation.previousUri;
+                nextRoundUri = response.round.navigation.nextUri;                
+            } else {
+                $scope.errorMessage = handleError(response.message);
+            }
+        };        
     }
 );
 
 var handleError = function (errorMessage) {
     return 'Oooops...something went wrong - ' + errorMessage;
 };
+

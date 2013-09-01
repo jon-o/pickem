3ee538e1814ae9d3c6bc08e1bdc9bfa7b98edea3
Jason <jason.d.ody@gmail.com>
2013-09-01 00:46:37 -0400

    Handling login error and redirect on successful login

--- App/Public/js/controllers/loginController.js
+++ App/Public/js/controllers/loginController.js
@@ -1,21 +1,46 @@
 'use strict';
 
 pickem.controller('LoginController',
-    function LoginController($scope, facebookService, pickemService) {
-        facebookService.login()
-            .then(function(fbResponse) {
-                //Sign into our api and send user data from FB
-                pickemService.login(fbResponse)
-                    .then(function(apiResponse) {
-                        alert(apiResponse);
-                    }, function(apiResponse) {
-                        //Something went wrong with the login to our API
-                        alert(apiResponse);
-                    }
-                );
-            }, function(fbResponse) {
-                //Something went wrong with the login to FB
-            }
-        );
+    function LoginController($scope, $rootScope, $location, facebookService, pickemService, 
+        sharedService) {
+            
+        $scope.loginVisible = false;
+        $scope.loginEnabled = false;
+        
+        $scope.login = function() {
+            $scope.loginEnabled = false;
+            authenticate();
+        };
+        
+        var authenticate = function() {
+            facebookService.login()
+                .then(function(fbResponse) {
+                    //Sign into our api and send user data from FB
+                    pickemService.login(fbResponse)
+                        .then(function(apiResponse) {
+                            $rootScope.isLoggedIn = true;
+                            $location.path('/games');
+                        }, function(apiResponse) {
+                            //Something went wrong with the login to our API
+                            enableLoginButton();
+                            sharedService.errorHandler.handleError(
+                                "Unsuccessful attempt to login to Pickem.");
+                        }
+                    );
+                }, function(fbResponse) {
+                    //Something went wrong with the login to FB
+                    enableLoginButton();
+                    sharedService.errorHandler.handleError(
+                        "Unsuccessful attempt to login to Facebook.");
+                }
+            );
+        };
+        
+        var enableLoginButton = function() {
+            $scope.loginVisible = true;
+            $scope.loginEnabled = true;
+        };
+        
+        authenticate();
     }
 );

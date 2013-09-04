b10a408070776dfa07f0c85a700e35dbe1b3a9e8
Jason <jason.d.ody@gmail.com>
2013-09-04 12:51:04 -0400

    Added login route

--- App/Public/js/app.js
+++ App/Public/js/app.js
@@ -4,6 +4,7 @@ var pickem = angular.module('pickem', ['$strap.directives'])
     .config(['$routeProvider', function($routeProvider) {
         
     $routeProvider
+        .when('/login', { templateUrl: 'partials/login-partial.html' })
         .when('/games', { templateUrl: 'partials/games-partial.html' })
         .when('/leaderboard', { templateUrl: 'partials/leaderboard-partial.html' })
         .otherwise({ redirectTo: '/games' });

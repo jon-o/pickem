0169846d8e5a27f0b78ba59a7ff9935fc2cad1b3
Jason <jason.d.ody@gmail.com>
2013-09-14 18:59:35 -0400

    Import games script using data from http://data2.7m.cn/matches_data/92/en/index.shtml

--- App/MySqlDataAccess/ImportGames.js
+++ App/MySqlDataAccess/ImportGames.js
@@ -1,12 +1,15 @@
 var config = require('../Config.js');
 var mysql = require('mysql');
 var fs = require('fs');
-var util = require('util');
 var byline = require('byline');
 var _ = require('underscore');
 var path = require('path');
+var util = require('util');
+require('datejs');
 
 var sourceFilePath = path.join(__dirname, 'Games.txt');
+var sourceTimeZone = "PST";
+var roundId = 6;
 
 var connection = mysql.createConnection({
     host: config.mysql.host,
@@ -19,30 +22,51 @@ var connection = mysql.createConnection({
 
 connection.query('SELECT Id AS id, Name as name FROM teams', function (err, rows) {
     if (err) {
-        console.log("**Error**");
-        console.log(err);
+        handleError(err, connection);
     } else {
         var teams = rows;
+        var insertStatement = 
+            'INSERT INTO games (DateAndTime, HomeTeamId, AwayTeamId, Result, RoundId, Score) VALUES';
         
         var stream = fs.createReadStream(sourceFilePath);
         stream = byline.createStream(stream);
-        
-        stream.on('end', function() {
-           console.log('FINISHED');
-        });
         
         var first = true;
         
         stream.on('data', function(line) {
             var fields = line.toString().split('\t');
             
-            console.log(getTeam(teams, fields[2]));
-            console.log(getTeam(teams, fields[4]));
+            var date = Date.parse(fields[1] + sourceTimeZone);
+            var homeTeam = getTeam(teams, fields[2]);
+            var awayTeam = getTeam(teams, fields[4])
+            
+            var insertValues = util.format("('%s', %d, %d, NULL, %d, NULL)", 
+                date.toISOString(), homeTeam.id, awayTeam.id, roundId);
+            
+            insertStatement = util.format('%s%s\n%s', insertStatement, !first ? ',' : '', insertValues);
             
             first = false;
         });
+        
+        stream.on('end', function() {
+            console.log(insertStatement);
+            connection.query(insertStatement, function(err, rows) {
+                if (err) {
+                    handleError(err, connection);
+                } else {
+                    connection.query('SELECT * FROM games WHERE roundId = ' + roundId, 
+                        function(err, rows) {
+                            if (err) {
+                                handleError(err, connection);
+                            } else {
+                                console.log(rows);
+                            }
+                            connection.destroy();
+                    });
+                }
+           });
+        });
     }
-    connection.destroy();
 });
 
 var getTeam = function(teams, name) {
@@ -51,4 +75,11 @@ var getTeam = function(teams, name) {
     });
     
     return team;
+};
+
+var handleError = function(err, connection) {
+    connection.destroy();
+    
+    console.log("**Error**");
+    console.log(err);
 };
